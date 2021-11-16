7ba674de53f0f0dc922591516e63b3f0fd329bb2
Chris Hannaby <chris.hannaby@netlify.com>
2021-11-16 15:18:12 -0700

    disable auto locale detection

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     locales: ["default", "en", "de", "fr"],
     defaultLocale: "default",
-    localeDetection: true,
+    localeDetection: false,
   },
   trailingSlash: true,
 };
